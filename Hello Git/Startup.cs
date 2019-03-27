73b868183acb841d1a7d77810f7ad7383de01442
Вадим <@DESKTOP-A8G532R>
2019-03-27 22:26:35 +0300

    111

--- Hello Git/Startup.cs
+++ Hello Git/Startup.cs
@@ -27,7 +27,7 @@ namespace Hello_Git
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Github!");
             });
         }
     }
